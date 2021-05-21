201f0fec35d13489d3326d831d1108450f190045
Choi jae <cjaeh0770@naver.com>
2021-05-21 18:37:31 +0900

    2021-05-21 jh commit

--- Team6_Born-chic/config/database.js
+++ Team6_Born-chic/config/database.js
@@ -8,7 +8,7 @@ module.exports = function () {
                 host: 'localhost',
                 user: 'root',
                 database: 'tutorial',
-                password: 'Eogus153@'
+                password: 'Wkdtlzm97!'
             })
         },
 
