edcff18aa1f025e620ca538557bdd7d255bbbbdb
KimDyun <swslooser@naver.com>
2021-05-21 18:12:34 +0900

    Git setting

--- Team6_Born-chic/routes/index.js
+++ Team6_Born-chic/routes/index.js
@@ -1,24 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit: 5,
-  host: 'localhost',
-  user: 'root',
-  database: 'tutorial',
-  password: 'Eogus153@'
-});
+var mysql_dbc = require('../config/database')();
+var connection = mysql_dbc.init();
+mysql_dbc.test_open(connection);
 
 router.get('/',function (req, res,next){
-  pool.getConnection(function (err, connection){
     var sqlForSelectList = "SELECT idx, creator_id, title, hit,content FROM board";
     connection.query(sqlForSelectList, function (err, rows){
       if (err) console.error("err : " + err);
       console.log("rows : "+ JSON.stringify(rows));
 
       res.render('index', {title: 'test', rows: rows});
-      connection.release();
     });
-  });
 });
 module.exports = router;
